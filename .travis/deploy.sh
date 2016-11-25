00bb391ac2ebbbffbd506b436e78b6d52f38ffbd
Paul Shibanov <paul.shibanov@gmail.com>
2016-11-25 11:58:15 +0400

    fix: git fetch before deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,5 +2,6 @@
 $(npm bin)/set-up-ssh --key "$encrypted_2265753103d1_key" --iv "$encrypted_2265753103d1_iv" --path-encrypted-key ".travis/github_deploy_key.enc"
 git config --global user.email "$GITHUB_EMAIL"
 git remote set-url origin git@github.com:paul-shibanov/trpo.git
+git fetch
 git push origin --delete gh-pages
 ng github-pages:deploy
