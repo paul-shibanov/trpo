51bf11d08439f4175d1d93568f2e604664f73e4d
Paul Shibanov <paul.shibanov@gmail.com>
2016-12-01 02:11:12 +0400

    refactor: node, tree creation Closes #23

--- src/app/tree/tree.ts
+++ src/app/tree/tree.ts
@@ -6,7 +6,7 @@ export class Tree {
   private _root: Node;
   private _p: number = 0; // nodes count;
   private _b: number = 0; // hanging nodes count;
-  private _depth: number = 0;
+  private _h: number = 0;
   private nodesArray: Node[] = []; // Links to each Node;
   private hangingNodesArray: Node[] = []; // Links to each hanging Node;
 
@@ -15,6 +15,13 @@ export class Tree {
       arr.push(node);
     };
   }
+  private _removeFromHanging(node: Node) {
+    const index = this.hangingNodesArray.indexOf(node);
+    if (index > -1) {
+      this.hangingNodesArray.splice(index, 1);
+      --this._b;
+    }
+  }
 
   private _buildLevel(buildQueue: Node[]): Node[] {
     let node: Node = null;
@@ -22,35 +29,21 @@ export class Tree {
     let nextBuildQueue: Node[] = [];
     while (buildQueue.length && this._p < this._n) {
       parent = buildQueue.shift();
-      node = new Node(this._p + 1, parent, this.calculateExpectedChilds(), this._depth);
+      node = new Node(this._p + 1, parent, this.calculateExpectedChilds(), this._h);
       this._p = this.nodesArray.push(node);
+      this._b = this.hangingNodesArray.push(node);
       if (isNull(parent)) {
         this._root = node;
-      }
-      if (node.mi === 0) {
-        node.hanging = true;
-        this._b = this.hangingNodesArray.push(node);
+      } else {
+        this._removeFromHanging(parent);
       }
       this._addNTimes(nextBuildQueue, node.mi, node);
     }
+    ++this._h;
     if (this._p >= this._n) {
-      this._hangNodes(this.nodesArray[this._p - 1].level - 1);
       return [];
     }
-    if (nextBuildQueue.length) {
-      ++this._depth;
-    }
     return nextBuildQueue;
-  }
-  private _hangNodes(lastLevel: number): void {
-    for (let i = this._p - 1, node: Node = this.nodesArray[i]; node && lastLevel <= node.level; --i) {
-      node = this.nodesArray[i];
-      if (node && !node.hasChild && !node.hanging) {
-        node.hanging = true;
-        this._b = this.hangingNodesArray.push(node);
-      }
-    }
-    this.hangingNodesArray.sort((a: Node, b: Node) => a.id - b.id);
   }
   private calculateExpectedChilds() {
     return this._mIsConst ? this._m : Math.floor(Math.random() * this._m);
@@ -60,6 +53,7 @@ export class Tree {
     while (buildQueue.length && this._p < this._n) {
       buildQueue = this._buildLevel(buildQueue);
     }
+    this.hangingNodesArray.sort((a: Node, b: Node) => a.id - b.id);
   }
   get root(): Node {
     return this._root;
@@ -71,7 +65,8 @@ export class Tree {
   }
   get nodes() {
     return this.nodesArray
-      .map((node: Node) => node.stringIdentifier);
+      .map((node: Node) => node.stringIdentifier)
+      .join(', ');
   }
   get alpha(): number {
     return this._p / this._b;
@@ -82,7 +77,7 @@ export class Tree {
   get b(): number {
     return this._b;
   }
-  get depth(): number {
-    return this._depth;
+  get h(): number {
+    return this._h;
   }
 }
